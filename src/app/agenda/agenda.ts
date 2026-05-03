b349b00c9c66759f133cd05865f315a9ae330638
Clara Jung <mail@clara-jung.de>
2026-05-03 13:48:08 +0200

    deployment fix 5

--- src/app/agenda/agenda.ts
+++ src/app/agenda/agenda.ts
@@ -14,12 +14,12 @@ interface AgendaItem {
 })
 export class Agenda {
   items: AgendaItem[] = [
-    { time: '11:00 Uhr',    title: 'Trauung',                        icon: '/assets/img/ringe.png'  },
-    { time: '12:00 Uhr',    title: 'Fotos im Kurpark',               icon: '/assets/img/kamera.png' },
-    { time: '13:00 Uhr',    title: 'Locationwechsel & Sektempfang',  icon: '/assets/img/sekt.png'   },
-    { time: '15:00 Uhr',    title: 'Kaffee & Kuchen',                icon: '/assets/img/kuchen.png' },
-    { time: '18:00 Uhr',    title: 'Abendessen',                     icon: '/assets/img/essen.png'  },
-    { time: '20:00 Uhr',    title: 'Hochzeitstanz',                  icon: '/assets/img/musik.png'  },
-    { time: 'Ab 20:30 Uhr', title: 'Party Open End',                 icon: '/assets/img/party.png' },
+    { time: '11:00 Uhr',    title: 'Trauung',                        icon: 'assets/img/ringe.png'  },
+    { time: '12:00 Uhr',    title: 'Fotos im Kurpark',               icon: 'assets/img/kamera.png' },
+    { time: '13:00 Uhr',    title: 'Locationwechsel & Sektempfang',  icon: 'assets/img/sekt.png'   },
+    { time: '15:00 Uhr',    title: 'Kaffee & Kuchen',                icon: 'assets/img/kuchen.png' },
+    { time: '18:00 Uhr',    title: 'Abendessen',                     icon: 'assets/img/essen.png'  },
+    { time: '20:00 Uhr',    title: 'Hochzeitstanz',                  icon: 'assets/img/musik.png'  },
+    { time: 'Ab 20:30 Uhr', title: 'Party Open End',                 icon: 'assets/img/party.png' },
   ];
 }
