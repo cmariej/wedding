e0946e054d93bf419fd55fde27844c433f515984
Clara Jung <mail@clara-jung.de>
2026-05-03 13:29:24 +0200

    fix deployment 3

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { provideRouter, withHashLocation, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -12,6 +12,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
       provideBrowserGlobalErrorListeners(),
       provideRouter(routes,
+      withHashLocation(),
       withInMemoryScrolling({
         scrollPositionRestoration: 'top'
       })),
